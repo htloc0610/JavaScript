8ba8b479f1e62d968fdc7261865bce1fe457979d
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-10-15 02:35:38 +0700

    2:35 15/10

---
+++ Bai36.DOM/main.js
@@ -0,0 +1,19 @@
+// console.log("GetElementsByTagName: ");
+// let li_item = document.getElementsByTagName("li");
+// console.log(li_item);
+// li_item[0].innerHTML = "Huhu";
+// let tmp = document.getElementById("agre");
+// console.log(tmp.innerHTML);
+
+let title = document.querySelector("#heading_title");
+title.innerHTML = "ahaa";
+console.log(title);
+
+// query -> phần tử đầu tiên trong doc phù hợp
+let titlee = document.querySelector(".list");
+titlee.innerHTML = "ahaa";
+console.log(titlee);
+
+let titleee = document.querySelector("li");
+titleee.innerHTML = "ahaae";
+console.log(titleee);
